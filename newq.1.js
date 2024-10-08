81483bb5ee2b41f2e96de0139be46291b79466d7
SajaluRahman <sajalurahman321@gmail.com>
2024-10-08 11:12:14 +0530

    question 9

---
+++ newq.1.js
@@ -0,0 +1,16 @@
+let a = 1;
+if (a >= 0 && a <= 11) {
+    console.log("Morning");
+}
+else if (a >= 12 && a <= 17) {
+    console.log("Afternoon");
+
+}
+else if (a >= 13 && a <= 23) {
+    console.log("Evening");
+
+}
+else {
+    console.log("Error");
+
+}
