d45bf797d32217f30c30344199cc3a739b4b4b42
SajaluRahman <sajalurahman321@gmail.com>
2024-10-14 10:02:25 +0530

    question 13

---
+++ newq.6.js
@@ -0,0 +1,15 @@
+let a= 655;
+if(a=="Green"){
+    console.log("GO");
+}
+   
+
+    else if(a=="yellow"){
+        console.log("Caution!");
+
+    }
+    else{
+        console.log("stop");
+
+    }
+    
