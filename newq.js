7fee0abe6b0884205daf909185164a61c7a1bfad
SajaluRahman <sajalurahman321@gmail.com>
2024-10-16 10:14:57 +0530

    question 15

---
+++ newq.js
@@ -0,0 +1,17 @@
+let a= "Monday";
+if(a=="Monday"){
+    console.log("Back to work!");
+}
+    else if(a=="Friday"){
+        console.log("Almost weekend!");
+
+    }
+    else if(a=="Saturday "){
+        console.log("Weekend!");
+
+    }
+    else{
+        console.log("Just another day!");
+
+    }
+    
