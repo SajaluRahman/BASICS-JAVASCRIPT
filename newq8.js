e5b030d0ffd0e4081ac81891055d268597427baa
SajaluRahman <sajalurahman321@gmail.com>
2024-10-18 11:04:09 +0530

    question 17

---
+++ newq8.js
@@ -0,0 +1,16 @@
+let x=12;
+if(x>10){
+    console.log("Greater than 10")
+}
+else{
+    console.log("less than 10")
+
+}
+// let a=40;
+// if(a%2==0){
+//     console.log("it is divisible by 2")
+// }
+// else{
+//     console.log("it is not divisible by 2")
+
+// }
