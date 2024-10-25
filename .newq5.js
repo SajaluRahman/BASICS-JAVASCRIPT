6982b38b207f1bada97cb0e2d595da77683b5b9a
SajaluRahman <sajalurahman321@gmail.com>
2024-10-25 10:40:45 +0530

    question 22

---
+++ .newq5.js
@@ -0,0 +1,17 @@
+let a= 86786;
+if(a=="sunny"){
+    console.log("Wear sunscreen");
+}
+    else if(a=="rainy"){
+        console.log("Bring an umbrella");
+
+    }
+    else if(a=="cloudy"){
+        console.log("Wear a light jacket");
+
+    }
+    else{
+        console.log("Check the forecast again");
+
+    }
+    
